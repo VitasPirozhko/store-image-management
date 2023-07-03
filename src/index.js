fdb2f1dcce80841cd15a13a6045b1c360af86181
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-07-03 12:46:16 +0300

    add loader and layout

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
-import axios from 'axios';
-
+const loader = document.querySelector('.js_loader');
 let csvFile = null;
+let archiveFile = null;
 
 const readSingleFile = (evt) => {
     csvFile = evt.target.files[0]; 
-    console.log(csvFile);
     const cvsLabel = document.querySelector('.uploadCvsInput');
-    console.log(cvsLabel);
     cvsLabel.textContent = csvFile.name;
 }
+
+const readArchive = (evt) => {
+    archiveFile = evt.target.files[0]; 
+    const archiveLabel = document.querySelector('.uploadArchiveInput');
+    archiveLabel.textContent = archiveFile.name;
+}
+
+const handleLoaderShow = (visible) => {
+    loader.style.display = visible ? 'flex' : 'none';
+};
 
 const sendCsv = () => {
     console.log(csvFile);
     // fetch('https://menutool-import-from-admin.menu-tool.glovoapp.com/store_images', {
-    //     method: "GET",
+    //     method: "POST",
     //     body: {
     //         file: csvFile,
     //     }
     // }).then((data) => {
     //     console.log(data);
     // })
+}
 
-    // axios.get('https://menutool-import-from-admin.menu-tool.glovoapp.com/store_images', {
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Authorization': 'Basic bWVudTopcDtcSnhONEZ1JFpOdkpW'
-    //     },
-    //     body: {
-    //         file: csvFile
-    //     },
-    // }).then((response) => {
-    //     console.log(response)
-    // })
+const sendArchive = () => {
+    let formData = new FormData();
+    formData.append('file', archiveFile);
+
+    handleLoaderShow(true);
+    fetch('https://menutool-import-from-admin.menu-tool.glovoapp.com/store_images', {
+        method: "POST",
+        headers: {
+            'Authorization': 'Basic bWVudTopcDtcSnhONEZ1JFpOdkpW',
+        },
+        body: formData,
+    }).catch((err) => {
+        console.log(err);
+    }).finally(() => {
+        handleLoaderShow(false);
+    })
 }
 
 document.getElementById('uploadCvsFile').addEventListener('change', readSingleFile);
+document.getElementById('uploadArchive').addEventListener('change', readArchive);
 document.getElementById('sendCsv').addEventListener('click', sendCsv);
+document.getElementById('sendArchive').addEventListener('click', sendArchive);
